1bf0af0964296c0238a1859ec87eecd9b5cecc73
Alex Gisby <alex.gisby@bbc.co.uk>
2016-06-30 11:57:16 +0100

    Unit tests for beforeQuery.

--- tests/ServiceTest.php
+++ tests/ServiceTest.php
@@ -6,7 +6,10 @@ use BBC\iPlayerRadio\Cache\Cache;
 use BBC\iPlayerRadio\WebserviceKit\NoResponseException;
 use BBC\iPlayerRadio\WebserviceKit\PHPUnit\GetMockedService;
 use BBC\iPlayerRadio\WebserviceKit\PHPUnit\TestCase;
+use BBC\iPlayerRadio\WebserviceKit\QueryInterface;
 use BBC\iPlayerRadio\WebserviceKit\Stubs\Monitoring;
+use BBC\iPlayerRadio\WebserviceKit\Stubs\OtherQuery;
+use BBC\iPlayerRadio\WebserviceKit\Stubs\Query;
 use Doctrine\Common\Cache\ArrayCache;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ConnectException;
@@ -71,6 +74,14 @@ class ServiceTest extends TestCase
 
         $this->assertEquals($newBreaker, $service->getCircuitBreaker());
         $this->assertEquals('yellow', $service->getCircuitBreaker()->mark);
+    }
+
+    public function testBeforeQuery()
+    {
+        $service = $this->getMockedService();
+        $this->assertEquals($service, $service->beforeQuery(function ($query) {
+            return $query;
+        }));
     }
 
     /* --------------- Empty Cache Tests ---------------------- */
@@ -592,5 +603,117 @@ class ServiceTest extends TestCase
 
         $this->assertEquals('foo', $responses[0]->message);
         $this->assertEquals('bar', $responses[1]->message);
+    }
+
+    /* ------------------ beforeQuery tests -------------------- */
+
+    public function testBeforeQueryNoTypehint()
+    {
+        $service = $this->getMockedService([
+            '{"message": "bar"}'
+        ]);
+        $query = $this->getMockedQuery();
+
+        $beforeCalled = false;
+        $service->beforeQuery(function ($query) use (&$beforeCalled) {
+            $beforeCalled = true;
+            return $query;
+        });
+
+        $service->fetch($query);
+        $this->assertTrue($beforeCalled);
+    }
+
+    public function testBeforeQueryExactTypehint()
+    {
+        $service = $this->getMockedService([
+            '{"message": "foo"}',
+        ]);
+        $query = $this->getMockedQuery();
+
+        $beforeCalled = false;
+        $service->beforeQuery(function (Query $query) use (&$beforeCalled) {
+            $beforeCalled = true;
+            return $query;
+        });
+
+        $service->fetch($query);
+        $this->assertTrue($beforeCalled);
+
+        // Doesn't match:
+
+        $service = $this->getMockedService([
+            '{"message": "bar"}',
+        ]);
+        $query = new OtherQuery();
+
+        $beforeCalled = false;
+        $service->beforeQuery(function (Query $query) use (&$beforeCalled) {
+            $beforeCalled = true;
+            return $query;
+        });
+
+        $service->fetch($query);
+        $this->assertFalse($beforeCalled);
+    }
+
+    public function testBeforeQueryParentClass()
+    {
+        $service = $this->getMockedService([
+            '{"message": "foo"}',
+        ]);
+        $query = $this->getMockedQuery();
+
+        $beforeCalled = false;
+        $service->beforeQuery(function (QueryInterface $query) use (&$beforeCalled) {
+            $beforeCalled = true;
+            return $query;
+        });
+
+        $service->fetch($query);
+        $this->assertTrue($beforeCalled);
+    }
+
+    public function testBeforeQueryMultipleHandlers()
+    {
+        $service = $this->getMockedService([
+            '{"message": "foo"}',
+        ]);
+        $query = $this->getMockedQuery();
+
+        $beforeCalled1 = false;
+        $beforeCalled2 = false;
+
+        $service->beforeQuery(function (Query $query) use (&$beforeCalled1) {
+            $beforeCalled1 = true;
+            return $query;
+        });
+
+        $service->beforeQuery(function ($query) use (&$beforeCalled2) {
+            $beforeCalled2 = true;
+            return $query;
+        });
+
+        $service->fetch($query);
+        $this->assertTrue($beforeCalled1);
+        $this->assertTrue($beforeCalled2);
+    }
+
+    /**
+     * @expectedException           \LogicException
+     * @expectedExceptionMessage    beforeHandlers must have at least one parameter!
+     */
+    public function testBeforeHandlerBadMethodSignature()
+    {
+        $service = $this->getMockedService([
+            '{"message": "foo"}',
+        ]);
+        $query = $this->getMockedQuery();
+
+        $service->beforeQuery(function () {
+            // Bad callback, needs a parameter.
+        });
+
+        $service->fetch($query);
     }
 }
